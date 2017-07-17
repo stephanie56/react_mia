13138599f2060da114e69addc43c26734ac1ceae
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-07-17 10:10:42 -0400

    Fixed bug

--- src/containers/Clock.js
+++ src/containers/Clock.js
@@ -3,11 +3,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Clock extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      clockTime: this.props.workDuration * 60
+    }
+    this._countDown = this._countDown.bind(this);
+  }
+
+  componentDidMount() {
+    const thiz = this
+    var time = thiz.state.clockTime;
+    var newClockTime = thiz._countDown(time);
+      setInterval(function(){
+        thiz.setState(
+          {clockTime: newClockTime}
+        )
+      }, 1000);
+  }
+
+  _countDown(time){
+    const timeLeft = time > 0 ? (time - 1) : time;
+    return timeLeft;
+  }
+
   render() {
     return(
       <div>
         <div>{ this.props.currentTask  }</div>
-        <div>{ this.props.workDuration  }</div>
+        <div>{ this.state.clockTime }</div>
         <div>
           <button>Start</button>
           <button>Stop</button>
@@ -19,7 +43,8 @@ class Clock extends Component {
 
 const mapStateToProps = (state) => ({
   currentTask: state.currentTask,
-  workDuration: state.workDuration
+  workDuration: state.workDuration,
+  breakDuration: state.breakDuration,
 });
 
 export default connect(mapStateToProps)(Clock);
