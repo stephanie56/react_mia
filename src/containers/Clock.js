5df1d2d2c52c2adcf2db879c0fc70453a1993fca
Communications <Communications>
2017-07-17 12:28:17 -0400

    Break time conversion method into two functions

--- src/containers/Clock.js
+++ src/containers/Clock.js
@@ -3,35 +3,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Clock extends Component {
-  constructor(props){
+
+  constructor(props) {
     super(props);
-    this.state = {
-      clockTime: this.props.workDuration * 60
-    }
-    this._countDown = this._countDown.bind(this);
+
+    // binding methods to this
+    this._convertToClockTime = this._convertToClockTime.bind(this);
   }
 
-  componentDidMount() {
-    const thiz = this
-    var time = thiz.state.clockTime;
-    var newClockTime = thiz._countDown(time);
-      setInterval(function(){
-        thiz.setState(
-          {clockTime: newClockTime}
-        )
-      }, 1000);
+  // method to convert workDuration / breakDuration to clockTime that includes Hours, Minutes and Seconds
+  // expect to return an object of numbers with the keys: hours, minutes, and seconds
+  _convertToClockTime(duration) {
+    // convert duration to hours
+    const hours = duration >= 60 ? (Math.floor(duration / 60)) : (0);
+    const minutes = totalSeconds >= 60 ? (totalSeconds / 60) : ("00");
+    const seconds = "00" ;
   }
 
-  _countDown(time){
-    const timeLeft = time > 0 ? (time - 1) : time;
-    return timeLeft;
+  // method to convert clockTime to displayedTime that shows Hours, Minutes and Seconds
+  // expect to return string "hh:mm:ss"
+  _convertToDisplayedTime(duration) {
+    const displayedHours = totalSeconds >= 3600 ? (totalSeconds / 3600) : ("00");
+    const displayedMinutes = totalSeconds >= 60 ? (totalSeconds / 60) : ("00");
+    const displayedSeconds = "00" ;
+
+    return `${displayedHours} : ${displayedMinutes} : ${displayedSeconds}`;
   }
 
   render() {
+    const clockTime = this._convertToDisplayedTime(this.props.workDuration);
     return(
       <div>
         <div>{ this.props.currentTask  }</div>
-        <div>{ this.state.clockTime }</div>
+        <div>{ clockTime }</div>
         <div>
           <button>Start</button>
           <button>Stop</button>
