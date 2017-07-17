07824dca4137b67e0057119ef02f234d503d734e
Communications <Communications>
2017-07-17 13:48:00 -0400

    Converted duration to time obj

--- src/containers/Clock.js
+++ src/containers/Clock.js
@@ -9,24 +9,40 @@ class Clock extends Component {
 
     // binding methods to this
     this._convertToClockTime = this._convertToClockTime.bind(this);
+    this._convertToDisplayedTime = this._convertToDisplayedTime.bind(this);
   }
 
   // method to convert workDuration / breakDuration to clockTime that includes Hours, Minutes and Seconds
   // expect to return an object of numbers with the keys: hours, minutes, and seconds
   _convertToClockTime(duration) {
     // convert duration to hours
-    const hours = duration >= 60 ? (Math.floor(duration / 60)) : (0);
-    const minutes = totalSeconds >= 60 ? (totalSeconds / 60) : ("00");
-    const seconds = "00" ;
+    let totalSeconds = duration * 60;
+    const timeObj = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+    if (totalSeconds >= 3600) {
+      timeObj.hours = Math.floor(totalSeconds / 3600);
+      totalSeconds -= 3600;
+    }
+    else if (totalSeconds >= 60) {
+      timeObj.minutes = Math.floor(totalSeconds / 60);
+      totalSeconds -= 60;
+    }
+    else {
+      timeObj.seconds = totalSeconds;
+    }
+    return timeObj;
   }
 
   // method to convert clockTime to displayedTime that shows Hours, Minutes and Seconds
   // expect to return string "hh:mm:ss"
-  _convertToDisplayedTime(duration) {
-    const displayedHours = totalSeconds >= 3600 ? (totalSeconds / 3600) : ("00");
-    const displayedMinutes = totalSeconds >= 60 ? (totalSeconds / 60) : ("00");
-    const displayedSeconds = "00" ;
-
+  _convertToDisplayedTime(val) {
+    const obj = this._convertToClockTime(val);
+    const displayedHours = obj.hours;
+    const displayedMinutes = obj.minutes;
+    const displayedSeconds = obj.seconds;
     return `${displayedHours} : ${displayedMinutes} : ${displayedSeconds}`;
   }
 
