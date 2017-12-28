ca54b351fa6dc96d644d444fa0522169d3770a9f
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-12-28 01:20:58 -0500

    Removed countdown code

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -4,53 +4,15 @@ class CountdownTimer extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      timeToCount: 0
-    }
 
     this._formatSeconds = this._formatSeconds.bind(this);
     this._addZero = this._addZero.bind(this);
   }
 
-  // fetch setDuration for countdown task
-  componentWillMount(){
-    this.setState({
-      timeToCount: this.props.setDuration * 60,
-    });
-  }
-
-  // start countdown
-  componentDidUpdate(prevState){
-    if(prevState.timeToCount !== this.state.timeToCount){
-      const that = this;
-      var timer = setInterval(function(){
-        if(that.state.timeToCount > 0 && that.props.isTimerOn){
-          that.setState({
-            timeToCount: that.state.timeToCount - 1
-          });
-          console.log(that.state.timeToCount);
-        }
-        else if(that.state.timeToCount > 0 && !that.props.isTimerOn){
-          clearInterval(timer);
-          console.log('stopped the timer');
-        }
-        else {
-          that.setState({
-            timeToCount: that.props.setDuration * 60
-          });
-          console.log("round 2!");
-        }
-      }, 1000);
-    }
-  }
-
-
-
   _formatSeconds(totalSeconds){
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
-
     return `${this._addZero(hours)} : ${this._addZero(minutes)} : ${this._addZero(seconds)}`;
   }
 
@@ -61,7 +23,7 @@ class CountdownTimer extends Component {
   render() {
     return(
         <div className="clock__timer">
-          <span>{ this._formatSeconds(this.state.timeToCount) }</span>
+          <span>{ this._formatSeconds(this.props.setDuration * 60) }</span>
         </div>
     )
   };
